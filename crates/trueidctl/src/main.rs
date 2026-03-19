999f42942aaa5063f9f7bfe5472067359d4cda3e
adilsaid64 <adilsaid64@gmail.com>
2026-03-19 21:32:15 +0000

    feat: setup a basic unix socket, got basic ctl talking with the dameon

--- crates/trueidctl/src/main.rs
+++ crates/trueidctl/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader, Write};
+use std::os::unix::net::UnixStream;
+
+use trueid_ipc::{Request, Response, SOCKET_PATH};
+
+fn main() -> std::io::Result<()> {
+    let mut stream = UnixStream::connect(SOCKET_PATH)?;
+    println!("Connected to daemon");
+
+    let request = Request::Ping;
+
+    let request_json = serde_json::to_string(&request).unwrap();
+
+    writeln!(stream, "{}", request_json)?;
+
+    let mut reader = BufReader::new(stream);
+    let mut line = String::new();
+
+    reader.read_line(&mut line)?;
+
+    let response: Response = serde_json::from_str(&line).unwrap();
+
+    println!("Response: {:?}", response);
+
+    Ok(())
 }
--- crates/trueidd/src/main.rs
+++ crates/trueidd/src/main.rs
@@ -2,11 +2,26 @@ use std::fs;
 use std::os::unix::net::UnixListener;
 use std::path::Path;
 
-use trueid_ipc::{SOCKET_PATH, Request, Response};
-use serde_json;
-use std::io::{BufRead, BufReader, Write};
+use trueid_ipc::SOCKET_PATH;
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    if Path::new(SOCKET_PATH).exists() {
+        fs::remove_file(SOCKET_PATH)?;
+    }
+
+    let listener = UnixListener::bind(SOCKET_PATH)?;
+    println!("TrueID daemon is running on {}", SOCKET_PATH);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("Client connected");
+            }
+            Err(err) => {
+                eprintln!("Error accepting connection: {}", err);
+            }
+        }
+    }
+
     Ok(())
 }
