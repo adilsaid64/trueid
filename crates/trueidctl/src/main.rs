092419681331dc720ae723772da2ec3be858a157
adilsaid64 <adilsaid64@gmail.com>
2026-03-21 15:14:56 +0000

    feat: setting up clap for cli tool

--- crates/trueidctl/src/main.rs
+++ crates/trueidctl/src/main.rs
@@ -1,26 +1,19 @@
-use std::io::{BufRead, BufReader, Write};
-use std::os::unix::net::UnixStream;
+use clap::Parser;
 
-use trueid_ipc::{Request, Response, SOCKET_PATH};
+#[derive(Parser)]
+#[command(name = "MyApp")]
+#[command(version = "1.0")]
+#[command(about = "Does awesome things", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    two: String,
+    #[arg(long)]
+    one: String,
+}
 
-fn main() -> std::io::Result<()> {
-    let mut stream = UnixStream::connect(SOCKET_PATH)?;
-    println!("Connected to daemon");
+fn main() {
+    let cli = Cli::parse();
 
-    let request = Request::Ping;
-
-    let request_json = serde_json::to_string(&request).unwrap();
-
-    writeln!(stream, "{}", request_json)?;
-
-    let mut reader = BufReader::new(stream);
-    let mut line = String::new();
-
-    reader.read_line(&mut line)?;
-
-    let response: Response = serde_json::from_str(&line).unwrap();
-
-    println!("Response: {:?}", response);
-
-    Ok(())
+    println!("two: {:?}", cli.two);
+    println!("one: {:?}", cli.one);
 }
