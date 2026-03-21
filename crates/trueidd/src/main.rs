29fee2fd529681f685ee1f7459cd433e92625404
adilsaid64 <adilsaid64@gmail.com>
2026-03-21 11:56:12 +0000

    feat: added request parsing and matching on dameon side, and some more code comments as i learn rust

--- crates/trueidd/src/main.rs
+++ crates/trueidd/src/main.rs
@@ -10,11 +10,15 @@ fn main() -> std::io::Result<()> {
         fs::remove_file(SOCKET_PATH)?;
     }
 
+    // create and bind the unix socket listener
+    // the dameon proceses will listen for connections
     let listener = UnixListener::bind(SOCKET_PATH)?;
     println!("TrueID daemon is running on {}", SOCKET_PATH);
 
+    // main loop to accepting connections and handling them
     for stream in listener.incoming() {
         match stream {
+            // on successful connection, we pass the stream to a handler function
             Ok(stream) => {
                 handle_connection(stream);
             }
@@ -30,8 +34,33 @@ fn main() -> std::io::Result<()> {
 
 fn handle_connection(mut stream: UnixStream) {
     use std::io::{Read, Write};
+    
+    // creates an empty buffer, an array of 1024 bytes all initialised to 0
     let mut buffer = [0; 1024];
-    let _ = stream.read(&mut buffer);
-    let response_json = serde_json::to_string(&Response::Pong).unwrap();
+    
+    // writes bytes to buffer
+    let result = stream.read(&mut buffer);
+    let bytes_read = result.unwrap();
+
+    // covert bytes to string
+    let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+    println!("Received message: {}", message);
+
+
+    let request = serde_json::from_str::<String>(&message);
+
+    let response = match request {
+        Ok(req) => {
+            println!("Parsed request: {}", req);
+            Response::Pong
+        }
+        Err(err) => {
+            eprintln!("Failed to parse request: {}", err);
+            Response::Error("Invalid request format".to_string())
+        }
+    };
+
+    // 
+    let response_json = serde_json::to_string(&response).unwrap();
     let _ = writeln!(stream, "{}", response_json);
 }
